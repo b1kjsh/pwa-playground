68d8a793d230be6a221aead9d8a3b10ca221cfde
b1kjsh <joshuaman32@gmail.com>
2019-05-23 21:17:32 -0600

    added upload component and started scaffolding the upload service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { UploadService } from './upload/upload.service';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
